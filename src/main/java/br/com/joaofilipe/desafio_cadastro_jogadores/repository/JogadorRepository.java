9ab14dce5f38630b453a4facf6eef6f6671857ef
João Filipe <filipeb@sefaz.pi.gov.br>
2024-12-12 08:54:54 -0300

    Finalizando JogadorService

--- src/main/java/br/com/joaofilipe/desafio_cadastro_jogadores/repository/JogadorRepository.java
+++ src/main/java/br/com/joaofilipe/desafio_cadastro_jogadores/repository/JogadorRepository.java
@@ -1,8 +1,11 @@
 package br.com.joaofilipe.desafio_cadastro_jogadores.repository;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
+import br.com.joaofilipe.desafio_cadastro_jogadores.model.GrupoCodinome;
 import br.com.joaofilipe.desafio_cadastro_jogadores.model.Jogador;
 
 @Repository
@@ -13,10 +16,19 @@ public class JogadorRepository {
         this.jdbcClient = jdbcClient;
     }
 
-    public void salvar(Jogador jogador) {
+    public Jogador salvar(Jogador jogador) {
         jdbcClient.sql("INSERT INTO jogadores (nome, email, telefone, codinome, grupo_codinome) VALUES (?, ?, ?, ?, ?)")
                 .params(jogador.nome(), jogador.email(), jogador.telefone(), jogador.codinome(),
                         jogador.grupoCodinome().getNome())
                 .update();
+
+        return jogador;
+    }
+
+    public List<String> listarCodinomesPorGrupo(GrupoCodinome grupoCodinome) {
+        return jdbcClient.sql("SELECT DISTINCT codinome FROM jogadores WHERE grupo_codinome = ?")
+                .param(grupoCodinome.name())
+                .query(String.class)
+                .list();
     }
 }
